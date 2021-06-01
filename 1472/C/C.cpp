8fed6d1e026271f46badf8b906c7962b2abe6fb9
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-01 17:32:50 +0700

    problem 1472C solved with DP

--- 1472/C/C.cpp
+++ 1472/C/C.cpp
@@ -2,10 +2,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void solve() {
+  long long n; cin >> n;
+  long long a[n]; long long score[n];
+  for(long long i=0; i<n; i++) cin >> a[i];
+  for(long long i=n-1; i>=0; i--) {
+    if(i+a[i]<n) {
+      score[i] = score[i+a[i]] + a[i];
+    } else {
+      score[i] = a[i];
+    }
+  }
+  cout << *max_element(score, score+n) << endl;
+}
+
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
+
+  long long t; cin >> t;
+  while(t--) {
+    solve();
+  }
 
 
   return 0;
