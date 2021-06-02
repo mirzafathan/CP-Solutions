728c2b5111f27cc0be8784a6ad4fb5f9b4895090
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-02 10:57:59 +0700

    full nyontek tutorial haha 991D

--- 991/D/D.cpp
+++ 991/D/D.cpp
@@ -1,11 +1,33 @@
+// Problem: https://codeforces.com/problemset/problem/991/D
 #include <bits/stdc++.h>
 using namespace std;
+
+void solve() {
+  string s[2]; cin >> s[0]; cin >> s[1];
+  long long len = s[0].length();
+  long long ans = 0;
+  long long all_empty_slot = 0;
+
+  for(long long i=0; i<len; i++) {
+    int current_empty_slot = (s[0][i]=='0') + (s[1][i]=='0');
+    all_empty_slot += current_empty_slot;
+    if(all_empty_slot>=3) {
+      ans++;
+      all_empty_slot -= 3;
+    } else {
+      all_empty_slot = current_empty_slot;
+    }
+  }
+
+  cout << ans << endl;
+}
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
 
+  solve();
 
   return 0;
 }
--- 991/D/README.md
+++ 991/D/README.md
@@ -1,4 +1,5 @@
 بسم الله الرحمن الرحيم
 <br />
 Problem: https://codeforces.com/problemset/problem/991/D
-![GitHub Logo](proof.png)
+![GitHub Logo](proof.png) <br />
+Source: https://codeforces.com/blog/entry/60181
