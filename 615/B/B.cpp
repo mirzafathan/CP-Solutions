5b0c44c6230111c4a37d0d5f387387e33621d391
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-03 23:51:25 +0700

    615b

--- 615/B/B.cpp
+++ 615/B/B.cpp
@@ -7,6 +7,35 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
+  long long n, m; cin >> n >> m;
+  long long a[2][m], seg_count[n], tail_length[n];
+  for(long long i=0; i<n; i++) seg_count[i] = 0;
+  for(long long i=0; i<m; i++) {
+    cin >> a[0][i] >> a[1][i];
+    seg_count[a[0][i]-1]++;
+    seg_count[a[1][i]-1]++;
+  }
+
+  tail_length[0] = 1;
+  for(long long i=1; i<n; i++) {
+    long long max_tail = 0;
+    for(long long j=0; j<m; j++) {
+      if((a[0][j]==(i+1) && (i+1)>a[1][j])) {
+        if(tail_length[a[1][j]-1]+1>max_tail) {
+          max_tail = tail_length[a[1][j]-1]+1;
+        }
+      } else if ((a[1][j]==i+1) && (i+1>a[0][j])) {
+        if(tail_length[a[0][j]-1]+1>max_tail) {
+          max_tail = tail_length[a[0][j]-1]+1;
+        }
+      }
+    }
+    tail_length[i] = max_tail;
+  }
+  long long ans[n];
+  for(long long i=0; i<n; i++) ans[i] = tail_length[i]*seg_count[i];
+
+  cout << *max_element(ans, ans+n) << endl;
 
   return 0;
 }
--- 615/B/README.md
+++ 615/B/README.md
@@ -1,4 +1,3 @@
 بسم الله الرحمن الرحيم
 <br />
 Problem: https://codeforces.com/problemset/problem/615/B <br/>
-![GitHub Logo](proof.png)
