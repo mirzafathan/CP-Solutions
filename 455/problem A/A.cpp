aa3f5761e0a910954fafcf69f1987894c6c71402
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-03 07:36:44 +0700

    455a(2)

--- 455/problem A/A.cpp
+++ 455/problem A/A.cpp
@@ -6,6 +6,21 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
+  long long n; cin >> n;
+  long long a[n];
+  for(long long i=0; i<n; i++) cin >> a[i];
+  long long maxel = *max_element(a,a+n);
+  long long cnt[maxel];
+  for(long long i=0; i<maxel; i++) cnt[i] = 0;
+  for(long long i=0; i<n; i++) cnt[a[i]-1]++;
 
+  long long ans[maxel+1];
+  ans[0] = 0;
+  ans[1] = cnt[0];
+  for(long long i=2; i<maxel+1; i++) {
+    ans[i] = max(ans[i-1], ans[i-2] + i*cnt[i-1]);
+  }
+
+  cout << ans[maxel] << endl;
   return 0;
 }
