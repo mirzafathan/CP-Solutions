a706e6d55fdcce62c33c2bfe506cf71dc940e10e
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-05 21:50:08 +0700

    1207 solved

--- 1207/C/C.cpp
+++ 1207/C/C.cpp
@@ -2,27 +2,28 @@
 //https://codeforces.com/problemset/problem/1207/C
 #include <bits/stdc++.h>
 using namespace std;
-const lmax = LLONG_MAX;
+const long long lmax = LLONG_MAX/2;
 
 void solve() {
   long long n, a, b; cin >> n >> a >> b;
   string s; cin >> s;
-  long long dp[n][2];
+  long long dp[n+1][2];
   dp[0][0] = b;
-  dp[0][1] = 9999;
-  cout << lmax << endl;
-/*
-  for(long long i=1; i<n; i++) {
-    if(s[i]=='1') {
-      if(s[i-1]=='0') {
-        dp[i][1] = min(dp[i-1][0]+2*(a+b), dp[i-1][1]+a+2*b);
-        dp[i][0] = 999
-      }
+  dp[0][1] = lmax;
 
+  for(long long i=1; i<n; i++) {
+    if(s[i]=='1' || s[i-1]=='1') {
+      dp[i][1] = min(dp[i-1][0]+2*(a+b), dp[i-1][1]+a+2*b);
+      dp[i][0] = lmax;
+    } else {
+      dp[i][1] = min(dp[i-1][0]+2*(a+b), dp[i-1][1]+2*b+a);
+      dp[i][0] = min(dp[i-1][0]+a+b, dp[i-1][1]+2*a+b);
     }
-    dp[i][0] = min(dp[i-1][0])
   }
-  */
+
+  dp[n][0] = min(dp[n-1][0]+a+b, dp[n-1][1]+b+2*a);
+
+  cout << dp[n][0] << endl;
 }
 
 int main() {
