6eacda218f3e5a5527b6e64e778e0d86eaaae862
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-06 23:50:08 +0700

    in queue

--- 1245/C/C.cpp
+++ 1245/C/C.cpp
@@ -8,6 +8,26 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
+  string s; cin >> s;
+  ll len = s.length();
+  ll ans;
+  ll dp[len];
+  dp[0] = 1;
+  for(ll i=1; i<len; i++) {
 
+    if(s[i]=='m' || s[i]=='w') {
+      cout << 0 << endl;
+      return 0;
+    }
+
+
+    if((s[i]=='u' && s[i-1]=='u') || (s[i]=='n' && s[i-1]=='n')){
+      if(i==1) dp[i] = 2;
+      else dp[i] = dp[i-1] + dp[i-2];
+    }
+    else dp[i] = dp[i-1];
+  }
+
+  cout << dp[len-1] << endl;
   return 0;
 }
