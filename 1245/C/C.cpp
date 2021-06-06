b724aea59c022608fa6f29fc98ec9256c82b2e56
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-07 00:45:02 +0700

    accepted 1245C

--- 1245/C/C.cpp
+++ 1245/C/C.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-
+const ll mod = 1000000007;
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
@@ -11,11 +11,12 @@ int main() {
   string s; cin >> s;
   ll len = s.length();
   ll ans;
-  ll dp[len];
+  ll dp[len+1];
   dp[0] = 1;
-  for(ll i=1; i<len; i++) {
+  dp[1] = 1;
+  for(ll i=1; i<len+1; i++) {
 
-    if(s[i]=='m' || s[i]=='w') {
+    if(s[i-1]=='m' || s[i-1]=='w') {
       cout << 0 << endl;
       return 0;
     }
@@ -23,7 +24,7 @@ int main() {
 
     if((s[i]=='u' && s[i-1]=='u') || (s[i]=='n' && s[i-1]=='n')){
       if(i==1) dp[i] = 2;
-      else dp[i] = dp[i-1] + dp[i-2];
+      else dp[i] = (dp[i-1]%mod + dp[i-2]%mod)%mod;
     }
     else dp[i] = dp[i-1];
   }
