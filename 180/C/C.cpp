6dbb26d0d7ae57782f4ab88d53235747dcd50adc
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-07 06:25:27 +0700

    180c

---
+++ 180/C/C.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/problemset/problem/180/C
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+
+  return 0;
+}
