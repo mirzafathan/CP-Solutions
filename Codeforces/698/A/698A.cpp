5f4f9ad3fd6ca7c30c56d27447c600de9aace6de
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-08 06:20:30 +0700

    acc 698a

--- Codeforces/698/A/698A.cpp
+++ Codeforces/698/A/698A.cpp
@@ -8,6 +8,25 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
+  ll n; cin >> n;
+  ll a[n+1];
+  for(ll i=1; i<=n; i++) cin >> a[i];
+
+  ll dp[n+1][3];
+  dp[0][0] = 0; dp[0][1] = 0; dp[0][2] = 0;
+
+  for(ll i=1; i<=n; i++) {
+    dp[i][0] = min(min(dp[i-1][0], dp[i-1][1]), dp[i-1][2])+1;
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]);
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]);
+    if(a[i]==0) {
+      dp[i][1]++; dp[i][2]++;
+    }
+    else if(a[i]==1) dp[i][2]++;
+    else if(a[i]==2) dp[i][1]++;
+  }
+
+  cout << min(min(dp[n][0], dp[n][1]), dp[n][2]) << endl;
 
   return 0;
 }
