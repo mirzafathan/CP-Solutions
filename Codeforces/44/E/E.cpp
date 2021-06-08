4fa4789c5939702d29f596520f03454b881397d9
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-08 10:19:00 +0700

    accepted anfisa

--- Codeforces/44/E/E.cpp
+++ Codeforces/44/E/E.cpp
@@ -8,7 +8,38 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
+  ll k, a, b; cin >> k >> a >> b;
+  string s; cin >> s;
+  ll len = s.length();
 
+  ll line = len/k;
+  ll mod = len%k;
+
+  if(floor((double)len/(double)k)<a||ceil((double)len/(double)k)>b) {
+    cout << "No solution" << endl;
+    return 0;
+  }
+
+  if(mod==0) {
+    for(ll i=0; i<len; i++) {
+      cout << s[i];
+      if((i+1)%line==0) cout << endl;
+    }
+    return 0;
+  }
+
+  ll idstart = 0;
+  for(ll i=0; i<k; i++) {
+
+    if(mod==0) {
+      cout << s.substr(idstart, line) << endl;
+      idstart += line;
+    } else {
+      cout << s.substr(idstart, line+1) << endl;
+      mod--;
+      idstart += line+1;
+    }
+  }
 
   return 0;
 }
