bce2dfa30a32c2978d32a560e2e94aca46af8f4b
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-08 08:11:36 +0700

    anfisa

---
+++ Codeforces/44/E/E.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/44/H
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+
+
+  return 0;
+}
---
+++ Codeforces/44/E/README.md
@@ -0,0 +1,5 @@
+بسم الله الرحمن الرحيم
+<br />
+# 44E
+### School Team Contest 2 (Winter Computer School 2010/11)
+Problem: https://codeforces.com/problemset/problem/44/E <br/>
