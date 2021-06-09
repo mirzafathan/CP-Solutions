5f7319b6fe39b4982137413aa47b5dce2584af6f
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-09 07:34:29 +0700

    191a

---
+++ Codeforces/191/A/README.md
@@ -0,0 +1,5 @@
+بسم الله الرحمن الرحيم
+<br />
+# 191A
+### Codeforces Round 121 (Div. 1)
+Problem: https://codeforces.com/problemset/problem/191/A<br/>
--- Codeforces/859/C/859C.cpp
+++ Codeforces/859/C/859C.cpp
@@ -1,3 +1,4 @@
+//https://codeforces.com/problemset/problem/859/C
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -7,6 +8,21 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
+  ll n; cin >> n;
+  ll pie[n];
+
+  for(ll i=0; i<n; i++) cin >> pie[i];
+
+  ll dp[n];
+  dp[n-1] = pie[n-1];
+  ll sum= pie[n-1];
+
+  for(ll i=n-2; i>=0; i--) {
+    sum+=pie[i];
+    dp[i] = max(dp[i+1], sum-dp[i+1]);
+  }
+
+  cout << sum-dp[0] << " " << dp[0] << endl;
 
   return 0;
 }
