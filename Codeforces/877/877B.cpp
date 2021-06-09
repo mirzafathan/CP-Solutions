e79d60a2a7555f8e0ff4b498f7b121358fb6b632
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-09 22:46:56 +0700

    accepted

--- Codeforces/877/877B.cpp
+++ Codeforces/877/877B.cpp
@@ -3,11 +3,18 @@
 using namespace std;
 typedef long long ll;
 
-int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
-
-
-  return 0;
+int main(){
+	string s; cin >> s;
+	ll n=s.size();
+	ll a1=0, a2=0, a3=0;
+	for(ll i=0;i<n;i++){
+		if(s[i]=='a'){
+			a1++;
+			a3++;
+		} else a2++;
+		a1 = max(a1,a2);
+		a2 = max(a2,a3);
+	}
+	cout << a1 << endl;
+	return 0;
 }
