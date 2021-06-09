ab9b84e9c0b7cc7c7c023c07c5c4f9361506c632
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-09 09:37:56 +0700

    ac 191a

--- Codeforces/191/A/191A.cpp
+++ Codeforces/191/A/191A.cpp
@@ -8,6 +8,33 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
+  ll n; cin >> n;
+  string king[n];
+  ll dp[26][26];
+
+  for(ll i=0; i<26; i++) {
+    for(ll j=0; j<26; j++) dp[i][j] = 0;
+  }
+
+  for(ll i=0; i<n; i++) {
+    ll left, right, len;
+    string kingi; cin >> kingi;
+    king[i] = kingi;
+    len = kingi.length();
+    left = kingi[0] - 'a';
+    right = kingi[len-1] - 'a';
+    for(ll j=0; j<26; j++) {
+      if(dp[j][left])
+        dp[j][right] = max(dp[j][right], dp[j][left]+len);
+    }
+    dp[left][right] = max(dp[left][right], len);
+  }
+
+  ll ans = 0;
+  for(ll i=0; i<26; i++)
+    ans = max(ans, dp[i][i]);
+
+  cout << ans << endl;
 
   return 0;
 }
