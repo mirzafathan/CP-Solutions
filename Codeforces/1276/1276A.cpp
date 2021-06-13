b8cab24feaed02b46eeb6e4556b7a41eb7075316
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-13 22:12:44 +0700

    628b

--- Codeforces/1276/1276A.cpp
+++ Codeforces/1276/1276A.cpp
@@ -7,6 +7,50 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
+  ll t; cin >> t;
+
+  while(t--) {
+    string s; cin >> s;
+    ll len = s.length();
+    ll ans = 0;
+    vector<ll> a;
+    for(ll i=0; i<len-2; i++) {
+      if(i+4<len) {
+        if(s[i]=='t' && s[i+1]=='w'
+        && s[i+2]=='o' && s[i+3]=='n' && s[i+4]=='e') {
+          ans++;
+          a.push_back(i+3);
+          i = i+4;
+        }
+      }
+      if(s[i]=='o' && s[i+1]=='n' && s[i+2]=='e') {
+        ans++;
+        a.push_back(i+2);
+        i = i+2;
+      }
+      if(s[i]=='t' && s[i+1]=='w' && s[i+2]=='o') {
+      //  cout << "ju" << endl;
+      //  cout << "ok"<< endl;
+      //  cout << i << endl;
+        ans++;
+        a.push_back(i+2);
+        i = i+2;
+      }
+
+    }
+    //cout << a.size()<< endl;
+    cout << ans << endl;
+    if(a.size()==0) {
+      cout << endl;
+    }
+    else {
+      for(ll i=0; i<a.size(); i++) {
+        cout << a[i];
+        if(i==a.size()-1) cout << endl;
+        else cout << " ";
+      }
+    }
+  }
 
   return 0;
 }
---
+++ Codeforces/628/README.md
@@ -0,0 +1,4 @@
+بسم الله الرحمن الرحيم
+<br />
+# Educational Codeforces Round 8
+### [628B](https://codeforces.com/problemset/problem/628/B)
