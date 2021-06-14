b4bf1039b2c5ab670255b01a382b538084daab4d
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-14 10:19:55 +0700

    still wrong

--- Codeforces/1282/B2.cpp
+++ Codeforces/1282/B2.cpp
@@ -1,12 +1,50 @@
+//https://codeforces.com/contest/1282/problem/B2
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+
+void solve() {
+  ll n, p, k, in; cin >> n >> p >> k;
+  vector<ll> a;
+  a.push_back(0);
+  for(ll i=1; i<=n; i++) {
+    cin >> in;
+    a.push_back(in);
+  }
+  sort(a.begin(), a.end());
+
+  ll dp[n+1];
+  dp[0] = 0;
+  for(ll i=1; i<=n; i++) {
+    if(i>=k) {
+      dp[i] = a[i] + dp[i-k];
+    } else {
+      dp[i] = a[1];
+    }
+  }
+
+  for(ll i=n; i>=0; i--) {
+    if(i<k && dp[i]<=p && i>0) {
+      cout << 1 << endl;
+      break;
+    }
+    if(dp[i]<=p) {
+      cout << i << endl;
+      break;
+    }
+  }
+}
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
 
+  ll t; cin >> t;
+
+  while(t--) {
+    solve();
+  }
 
   return 0;
 }
