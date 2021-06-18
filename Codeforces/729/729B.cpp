1ddcf5e294372054e918971bc492d99414bc9c9b
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-18 14:00:53 +0700

    729 b was just too easy :|

--- Codeforces/729/729B.cpp
+++ Codeforces/729/729B.cpp
@@ -8,6 +8,51 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
+  ll n, m; cin >> n >> m;
+  ll ans = 0;
+
+  ll a[n+1][m+1];
+
+  for(ll i=1; i<=n; i++) {
+    for(ll j=1; j<=m; j++) {
+      cin >> a[i][j];
+    }
+  }
+
+  for(ll i=1; i<=n; i++) {
+    bool f = false;
+    for(ll j=1; j<=m; j++) {
+      if(a[i][j]==1) f = true;
+      if(f && a[i][j]==0) ans++;
+    }
+  }
+
+  for(ll i=1; i<=n; i++) {
+    bool f = false;
+    for(ll j=m; j>=1; j--) {
+      if(a[i][j]==1) f = true;
+      if(f && a[i][j]==0) ans++;
+    }
+  }
+
+  for(ll j=1; j<=m; j++) {
+    bool f = false;
+    for(ll i=1; i<=n; i++) {
+      if(a[i][j]==1) f = true;
+      if(f && a[i][j]==0) ans++;
+    }
+  }
+
+
+  for(ll j=1; j<=m; j++) {
+    bool f = false;
+    for(ll i=n; i>=1; i--) {
+      if(a[i][j]==1) f = true;
+      if(f && a[i][j]==0) ans++;
+    }
+  }
+
+  cout << ans << endl;
 
   return 0;
 }
