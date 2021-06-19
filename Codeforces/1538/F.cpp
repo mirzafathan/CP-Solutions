6cd9cbb40a8e7600e89a1f5bee38d5a27cd0b17f
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-19 21:32:33 +0700

    1588F ac

--- Codeforces/1538/F.cpp
+++ Codeforces/1538/F.cpp
@@ -2,11 +2,30 @@
 using namespace std;
 typedef long long ll;
 
+void solve() {
+  ll l, r; cin >> l >> r;
+
+  ll changes = 0;
+
+  while(r-l>0) {
+    changes+= r - l;
+    r/=10;
+    l/=10;
+  }
+
+  cout << changes << endl;
+}
+
+
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
 
+  ll t; cin >> t;
+  while(t--) {
+    solve();
+  }
 
   return 0;
 }
