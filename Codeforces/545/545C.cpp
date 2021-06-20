51e868bb7e185c0c0dd292b9b031190389543225
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-20 11:54:59 +0700

    545c ac

--- Codeforces/545/545C.cpp
+++ Codeforces/545/545C.cpp
@@ -8,6 +8,43 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
+  ll n; cin >> n;
+  ll trees[n+1];
+  ll height[n+1];
+
+  trees[0] = height[0] = 0;
+
+  for(ll i=1; i<=n; i++)
+    cin >> trees[i] >> height[i];
+
+  ll dpl[n+1], dps[n+1], dpr[n+1];
+
+  for(ll i=0; i<=n; i++) {
+    dpl[i] = dps[i] = dpr[i] = 0;
+  }
+
+  for(ll i=1;i<n;i++) {
+    dps[i] = max(max(dpl[i-1],dps[i-1]),dpr[i-1]);
+    if(i==1) {
+      dpl[i] = 1;
+      continue;
+    }
+    if(trees[i]-trees[i-1]>height[i]) {
+      dpl[i] = max(dps[i-1], dpl[i-1])+1;
+    }
+    if(trees[i]-trees[i-1]-height[i-1]>height[i]) {
+      dpl[i] = max(max(dpl[i], dpr[i-1]+1), dps[i-1]+1);
+    }
+    if(trees[i+1]-trees[i]>height[i]) {
+      dpr[i] = max(max(dpl[i-1],dps[i-1]),dpr[i-1])+1;
+    }
+  }
+
+  dpr[n] = max(max(dps[n-1], dpr[n-1]), dpl[n-1])+1;
+
+  cout << dpr[n] << endl;
+
+
 
   return 0;
 }
--- Codeforces/545/README.md
+++ Codeforces/545/README.md
@@ -1,5 +1,7 @@
 بسم الله الرحمن الرحيم
 <br />
 # Codeforces Round #303 (Div. 2)
-### [545C](https://codeforces.com/problemset/problem/545/C) <br/><br/>
+### [545C](https://codeforces.com/problemset/problem/545/C)
+Dynamic Programming: Store the value of Stay[i], Right[i], and Left[i] where they store the maximum value of fallen trees from 1st to the *i*-th tree if the *i*-th tree is stay, fallen to the right side, or fallen to the left side, respectively.
+<br/><br/>
 **Muhammad Mirza Fathan | 2021**
