c7811bc3e2bfc0c5253522ab75ec0ce42f6d4c82
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-22 23:28:58 +0700

    half bismillah

--- CSES.FI/Graph Algorithms/monsters.cpp
+++ CSES.FI/Graph Algorithms/monsters.cpp
@@ -1,121 +1,141 @@
+// Muhammad Mirza Fathan
 #include <bits/stdc++.h>
-#define endl "\n"
+#define fr front
+#define pb push_back
+#define mp make_pair
 using namespace std;
-#define int long long int
-#define watch(x) cout << (#x) << " is " << (x) << "\n"
-#define watch2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << "\n"
+typedef long long ll;
+vector< vector<ll> > time_stamp;
+vector< vector< pair<ll,ll> > > parents;
+vector< pair<ll,ll> > monsters;
+ll n, m;
 
-int n, m;
+/* MOVES */
 
-vector<vector<pair<int,int>>> path;
-vector<vector<bool>> vis;
+pair<ll,ll> left(pair<ll,ll> point) {
+	point.second--;
+	return point;
+}
 
-int sx, sy, ex, ey;
+pair<ll,ll> right(pair<ll,ll> point) {
+	point.second++;
+	return point;
+}
 
-vector<pair<int,int>> moves = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+pair<ll,ll> up(pair<ll,ll> point) {
+	point.first--;
+	return point;
+}
 
-bool isValid(int x, int y)
-{
-	if(x<0 or x >= n or y <0 or y>=m)
+pair<ll,ll> down(pair<ll,ll> point) {
+	point.first++;
+	return point;
+}
+
+/* ****** */
+
+/* CHECK MOVE */
+
+bool isValid(pair<ll,ll> point, ll stopwatch) {
+	ll y = point.first;
+	ll x = point.second;
+	if(y<0 || y>=n || x<=0 || x>=m)
 		return false;
-	if(vis[x][y])
+	if(time_stamp[y][x]<= stopwatch)
 		return false;
 	return true;
 }
 
-void bfs()
-{
-	queue<pair<int,int>> q;
-	q.push({sx,sy});
-	while(!q.empty())
-	{
-		int cx = q.front().first;
-		int cy = q.front().second;
+/* ****** */
+
+/* MONSTERS BFS */
+
+void monsters_bfs(pair<ll,ll> monster) {
+
+	queue< pair< pair<ll,ll>, ll> > q;
+	q.push(mp(monster, 0));
+	while(!q.empty()) {
+		pair<ll,ll> cur = q.fr().first;
+		ll curtime = q.fr().second;
+		time_stamp[cur.first][cur.second] = curtime;
 		q.pop();
-		for(auto mv: moves)
-		{
-			int mvx = mv.first;
-			int mvy = mv.second;
-			if(isValid(cx+mvx, cy+mvy))
-			{
-				q.push({cx+mvx, cy+mvy});
-				vis[cx+mvx][cy+mvy] = true;
-				path[cx+mvx][cy+mvy] = {mvx,mvy};
-			}
-		}
+		if(isValid(left(cur), curtime+1))
+			q.push(mp(left(cur), curtime+1));
+		if(isValid(right(cur), curtime+1))
+			q.push(mp(right(cur), curtime+1));
+		if(isValid(up(cur), curtime+1))
+			q.push(mp(up(cur), curtime+1));
+		if(isValid(down(cur), curtime+1))
+			q.push(mp(down(cur), curtime+1));
 	}
+
 }
 
-int32_t main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+/* ****** */
+
+/* ESCAPE ALGORITHM */
+
+bool escape(pair<ll,ll> start) {
+	return false;
+}
+
+/* ****** */
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+
 	cin >> n >> m;
-	path.resize(n);
-	vis.resize(n);
-	for(int i = 0; i < n; ++i)
-	{
-		path[i].resize(m);
-		vis[i].resize(m);
-	}
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			path[i][j] = {-1,-1};
-			char c; cin >> c;
-			if(c == '#')
-			{
-				vis[i][j] = true;
-			}
-			if(c == 'A')
-			{
-				sx = i; sy = j;
-			}
-			if(c == 'B')
-			{
-				ex = i; ey = j;
-			}
-		}
-	}
-	bfs();
 
-	if(!vis[ex][ey])
-	{
-		cout << "NO" << endl;
-		return 0;
-	}
-	cout << "YES" << endl;
 
-	vector<pair<int,int>> ans;
-	pair<int,int> end = {ex,ey};
+	pair<ll,ll> start;
 
-	while(end.first != sx or end.second != sy)
-	{
-		ans.push_back(path[end.first][end.second]);
-		end.first -= ans.back().first;
-		end.second -= ans.back().second;
+
+	parents.resize(n);
+	time_stamp.resize(n);
+
+
+	for(ll i=0; i<n; i++) {
+		parents[i].resize(m);
+		time_stamp[i].resize(m);
 	}
 
-	reverse(ans.begin(), ans.end());
-	cout << ans.size() << endl;
-	for(auto c: ans)
-	{
-		if(c.first == 1 and c.second ==0)
-		{
-			cout << 'D';
-		}
-		else if(c.first == -1 and c.second ==0)
-		{
-			cout << 'U';
-		}
-		else if(c.first == 0 and c.second == 1)
-		{
-			cout << 'R';
-		}
-		else if(c.first == 0 and c.second == -1)
-		{
-			cout << 'L';
+
+	for(ll i=0; i<n; i++) {
+		for(ll j=0; j<m; j++) {
+			time_stamp[i][j] = LLONG_MAX;
 		}
 	}
+
+
+	for(ll i=0; i<n; i++) {
+		for(ll j=0; j<m; j++) {
+			char yx; cin >> yx;
+			if(yx=='#') time_stamp[i][j] = -1;
+			if(yx=='M') monsters.pb(mp(i,j));
+			if(yx=='A') start = mp(i,j);
+		}
+	}
+
+	/* MONSTERS PROCESSING */
+
+	for(ll i=0; i<monsters.size(); i++)
+		monsters_bfs(monsters[i]);
+
+	/* ****** */
+
+	for(ll i=0; i<n; i++) {
+		for(ll j=0; j<m; j++) {
+			cout << time_stamp[i][j] << " ";
+		} cout << endl;
+	}
+
+
+
+
+
+
+  return 0;
 }
