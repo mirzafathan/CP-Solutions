c62c3244cfd42593398105ed7a4831f54b85cfa1
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-24 17:02:34 +0700

    1536c

---
+++ Codeforces/1536/C.cpp
@@ -0,0 +1,16 @@
+// Muhammad Mirza Fathan 2021
+#include <bits/stdc++.h>
+using namespace std;
+#define fr front
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+
+  return 0;
+}
---
+++ Codeforces/1536/README.md
@@ -0,0 +1,5 @@
+بسم الله الرحمن الرحيم
+<br />
+# Codeforces Round #724 (Div. 2)
+### [1536C](https://codeforces.com/problemset/problem/1536/C) <br/><br/>
+**Muhammad Mirza Fathan | 2021**
