ec0ed34b3eada12b16e8860fb1daa10e2cda5755
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-24 21:03:23 +0700

    1536c accepted

--- Codeforces/1536/C.cpp
+++ Codeforces/1536/C.cpp
@@ -6,10 +6,49 @@ using namespace std;
 #define mp make_pair
 typedef long long ll;
 
+/************************/
+/* TEMPLATES */
+ll gcd(ll a, ll b)
+{
+    if (a == 0)
+      return b;
+    if (b == 0)
+      return a;
+
+    if (a == b)
+      return a;
+    if (a > b)
+        return gcd(a%b, b);
+    return gcd(a, b%a);
+}
+
+/************************/
+
+
+void solve() {
+  ll n; cin >> n;
+  string s; cin >> s;
+  ll d=0, k=0;
+  map<pair<ll,ll>, ll> hash;
+
+  for(ll i=0; i<s.length(); i++) {
+    if(s[i]=='D') d++;
+    else k++;
+    ll x = gcd(d,k);
+    cout << ++hash[mp(d/x, k/x)] << " ";
+  } cout << endl;
+
+}
+
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
+
+  ll t; cin >> t;
+  while(t--) {
+    solve();
+  }
 
 
   return 0;
--- Codeforces/template/template.cpp
+++ Codeforces/template/template.cpp
@@ -6,6 +6,24 @@ using namespace std;
 #define mp make_pair
 typedef long long ll;
 
+/************************/
+/* TEMPLATES */
+ll gcd(ll a, ll b)
+{
+    if (a == 0)
+      return b;
+    if (b == 0)
+      return a;
+
+    if (a == b)
+      return a;
+    if (a > b)
+        return gcd(a%b, b);
+    return gcd(a, b%a);
+}
+
+/************************/
+
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
