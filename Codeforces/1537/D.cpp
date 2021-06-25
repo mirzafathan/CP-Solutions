053bb5e222bd4a8b2abc4e5dd8771bb4415dcd12
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-06-25 10:31:52 +0700

    1537d

--- Codeforces/1537/D.cpp
+++ Codeforces/1537/D.cpp
@@ -1,13 +1,128 @@
 // Muhammad Mirza Fathan 2021
 #include <bits/stdc++.h>
 using namespace std;
+
+/************************/
+/* TEMPLATES */
+
+#define fr front
+#define bk back
+#define pb push_back
+#define mp make_pair
+#define sc second
+#define fi first
 typedef long long ll;
+# define ll long long
+# define ld long double
+# define FOR(i,x,y) for(LL i=x;i<y;i++)
+# define RFOR(i,x,y) for(LL i=x;i>=y;i--)
+# define DEBUG_STL(x) for(auto i:x){cout<<i<<" ";}cout<<endl;
+# define DEBUG_1D(x,a,b) FOR(i,a,b){cout<<x[i]<<" ";}cout<<endl;
+# define DEBUG_2D(x,a1,b1,a2,b2) FOR(i,a1,b1){FOR(j,a2,b2){cout<<x[i][j]<<" ";}cout<<endl;}
+# define ALL(x) x.begin(), x.end()
+# define MOD (ll)(1e9+7)
+# define INF8 (ll)(1e17+5)
+# define endl '\n'
+
+typedef vector<ll> VI;
+typedef vector<bool> VB;
+typedef vector<string> VS;
+typedef vector<vector<ll> > VVI;
+typedef pair<ll,ll> PI;
+typedef pair<ll,PI > PPI;
+typedef vector<PI> VP;
+typedef vector<PPI> VPP;
+
+ll mod(ll x) {
+  return (x%MOD + MOD)%MOD;
+}
+
+ll power(ll x, ll y) {
+  ll res = 1;
+  x = x%MOD;
+  if (x == 0) return 0;
+  while (y > 0) {
+    if (y&1) res = mod(res * x);
+    y >>= 1; x = mod(x * x);
+  }
+  return res;
+}
+
+ll gcd(ll a, ll b)
+{
+    if (a == 0)
+      return b;
+    if (b == 0)
+      return a;
+
+    if (a == b)
+      return a;
+    if (a > b)
+        return gcd(a%b, b);
+    return gcd(a, b%a);
+}
+
+/* From hu_tao:
+
+        Random stuff to try when stuck:
+           -if it's 2C then it's dp
+           -for combo/probability problems, expand the given form we're interested in
+           -make everything the same then build an answer (constructive, make everything 0 then do something)
+           -something appears in parts of 2 --> model as graph
+           -assume a greedy then try to show why it works
+           -find way to simplify into one variable if multiple exist
+           -treat it like fmc (note any passing thoughts/algo that could be used so you can revisit them)
+           -find lower and upper bounds on answer
+           -figure out what ur trying to find and isolate it
+           -see what observations you have and come up with more continuations
+           -work backwards (in constructive, go from the goal to the start)
+           -turn into prefix/suffix sum argument (often works if problem revolves around adjacent array elements)
+           -instead of solving for answer, try solving for complement (ex, find n-(min) instead of max)
+           -draw something
+           -simulate a process
+           -dont implement something unless if ur fairly confident its correct
+           -after 3 bad submissions move on to next problem if applicable
+           -do something instead of nothing and stay organized
+           -write stuff down
+        Random stuff to check when wa:
+           -if code is way too long/cancer then reassess
+           -switched N/M
+           -int overflow
+           -switched variables
+           -wrong MOD
+           -hardcoded edge case incorrectly
+        Random stuff to check when tle:
+           -continue instead of break
+           -condition in for/while loop bad
+        Random stuff to check when rte:
+           -switched N/M
+           -long to int/int overflow
+           -division by 0
+           -edge case for empty list/data structure/N=1
+     */
+
+/************************/
+
+void solve() {
+
+}
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
 
+  /*
+  // for multiple testcase problems
+
+  ll t; cin >> t;
+  while(t--) {
+    solve();
+  }
+
+  */
+
+  solve();
 
   return 0;
 }
--- Codeforces/1537/README.md
+++ Codeforces/1537/README.md
@@ -1,8 +1,10 @@
 بسم الله الرحمن الرحيم
 <br />
 # Codeforces Round #726 (Div. 2)
+## Solved
 ### [1537A](https://codeforces.com/problemset/problem/1537/A)
 ### [1537B](https://codeforces.com/problemset/problem/1537/B)
 ### [1537C](https://codeforces.com/problemset/problem/1537/C)
+### [1537D](https://codeforces.com/problemset/problem/1537/D)
 ### [1537E1](https://codeforces.com/problemset/problem/1537/E1) <br/><br/>
 **Muhammad Mirza Fathan | 2021**
