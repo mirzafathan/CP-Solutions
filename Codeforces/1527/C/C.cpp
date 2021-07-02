c9b3540ed1df9ce7603c172927a22fc014934922
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-07-02 21:39:52 +0700

    edit 1527c

--- Codeforces/1527/C/C.cpp
+++ Codeforces/1527/C/C.cpp
@@ -10,17 +10,6 @@ int main() {
     cin.tie(0);
     cout.tie(0);
 
-    long long t; cin >> t;
-    for(int i=0; i<t; i++) {
-      long long n; cin >> n;
-      string s; cin >> s;
-      long long len = s.length();
-      bool zero_more_than_one = 0;
-      for(int j=0; j<(len-1)/2; j++) {
-        if(s[j]=='0') {zero_more_than_one = 1; break;}
-      }
-      if(len%2==1 && s[(len-1)/2]!='1' && len>1 && zero_more_than_one) cout << "ALICE" << endl;
-      else cout << "BOB" << endl;
-    }
+    
     return 0;
 }
