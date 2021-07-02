e41d79dc78a56e973ae14fe58c0e3128638d05c7
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-07-02 22:32:20 +0700

    ac

--- Codeforces/1527/C/C.cpp
+++ Codeforces/1527/C/C.cpp
@@ -1,15 +1,39 @@
-//https://codeforces.com/problemset/problem/1527/B1
+//https://codeforces.com/problemset/problem/1527/C
 #include <bits/stdc++.h>
 
 using namespace std;
 
+void solve() {
+  long long n; cin >> n;
+  vector<long long> a(n+1);
 
+  for(long long i=1; i<=n; i++)
+    cin >> a[i];
+
+  vector<long long> dp(n+1);
+  map<long long, long long> hash;
+
+  long long ans = 0;
+
+  dp[0] = 0;
+  for(long long i=1; i<=n; i++) {
+    dp[i] = dp[i-1] + hash[a[i]];
+    hash[a[i]] += i;
+    ans += dp[i];
+  }
+
+  cout << ans << endl;
+
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    
+    long long t; cin >> t;
+
+    while(t--) solve();
+
     return 0;
 }
