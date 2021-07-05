eeccdf3c0030c44ea6191bf1edd0b8774c3b30e0
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-07-05 19:10:14 +0700

    ac 1513b

--- Codeforces/1513/B/B.cpp
+++ Codeforces/1513/B/B.cpp
@@ -2,37 +2,75 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define MOD (ll)(1e9+7)
+# define INF8 (ll)(1e17+5)
 
-const long long mod = 1000000007;
-long long dp[200000];
+ll mod(ll x) {
+  return (x%MOD + MOD)%MOD;
+}
+
+ll fact(ll x) {
+  if(x<1) {
+    return 1;
+  }
+  if(x==1) {
+    return 1;
+  } else {
+    ll ans = 1;
+    ll i=1;
+    while(i<=x) {
+      ans = ans*i;
+      ans = mod(ans);
+      i++;
+    }
+    return ans;
+  }
+}
 
 void solve() {
-  long long n, m;
-  cin >> n >> m;
-  long long ans = 0;
-  while(n) {
-    long long last = n%10;
-    if(last+m<10) ans+=1;
-    else ans+=dp[m+last-10];
-    ans= ans%mod;
-    n/=10;
+  ll n; cin >> n;
+  vector<ll> a(n);
+
+  ll minm = INF8;
+  ll cnt = 0;
+
+  for(ll i=0; i<n; i++) {
+    cin >> a[i];
+    if(a[i]<minm) {
+      cnt = 1;
+      minm = a[i];
+      continue;
+    }
+    if(a[i]==minm) {
+      cnt++;
+    }
   }
-  cout << ans << endl;
+
+  if(cnt<2) {
+    cout << 0 << endl;
+    return;
+  }
+
+  bool supermask = true;
+  for(ll i=0; i<a.size(); i++) {
+    if((a[i]&minm)!=minm) supermask = false;
+  }
+
+  if(supermask) {
+    cout << mod((cnt)*(cnt-1)*(fact(n-2))) << endl;
+  } else {
+    cout << 0 << endl;
+  }
+
 }
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  long long t; cin >> t;
 
-  for(int i=0; i<200000; i++) {
-    if(i<9) dp[i] = 2;
-    else if(i==9) dp[i] = 3;
-    else {
-      dp[i] = (dp[i-9] + dp[i-10])%mod;
-    }
-  }
+  long long t; cin >> t;
 
   while(t--) {
     solve();
--- Codeforces/1513/B/README.md
+++ Codeforces/1513/B/README.md
@@ -1,3 +1,4 @@
 بسم الله الرحمن الرحيم
 <br />
-Problem: https://codeforces.com/problemset/problem/1513/C 
+Problem: https://codeforces.com/problemset/problem/1513/B
+Bitmask. 
