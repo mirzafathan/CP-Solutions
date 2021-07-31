5bdd7ae0b9463975e16018efed7036566472ce78
Mirza Fathan <13518111@std.stei.itb.ac.id>
2021-07-31 15:20:00 +0700

    msh tle

--- Codeforces/1555/D.cpp
+++ Codeforces/1555/D.cpp
@@ -2,9 +2,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/************************/
-/* TEMPLATES */
-
 #define fr front
 #define bk back
 #define pb push_back
@@ -32,117 +29,35 @@ typedef pair<ll,PI > PPI;
 typedef vector<PI> VP;
 typedef vector<PPI> VPP;
 
-vector<ll> primeList;
-vector<bool> primes;
-
-
-ll mod(ll x) {
-  return (x%MOD + MOD)%MOD;
-}
-
-ll power(ll x, ll y) {
-  ll res = 1;
-  x = x%MOD;
-  if (x == 0) return 0;
-  while (y > 0) {
-    if (y&1) res = mod(res * x);
-    y >>= 1; x = mod(x * x);
-  }
-  return res;
-}
-
-ll gcd(ll a, ll b)
-{
-    if (a == 0)
-      return b;
-    if (b == 0)
-      return a;
-
-    if (a == b)
-      return a;
-    if (a > b)
-        return gcd(a%b, b);
-    return gcd(a, b%a);
-}
-
-void sieve() {
-  primes[0] = false;
-  primes[1] = false;
-  for(ll i=2; i<primes.size(); i++) primes[i] = true;
-
-  for(ll i=2; i*i<primes.size(); i++) {
-    if(primes[i]) {
-      for(ll j=i*i; j<primes.size(); j+=i)
-        primes[j] = false;
-    }
-  }
-  for(ll i=2; i<primes.size(); i++) {
-    if(primes[i]) primeList.push_back(i);
-  }
-
-}
-
-bool isPrime(ll n)
-{
-    if (n <= 1)
-        return false;
-    if (n <= 3)
-        return true;
-
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-
-    for (ll i = 5; i * i <= n; i = i + 6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
-
-    return true;
-}
-
-
-/* From hu_tao:
-
-        Random stuff to try when stuck:
-           -if it's 2C then it's dp
-           -for combo/probability problems, expand the given form we're interested in
-           -make everything the same then build an answer (constructive, make everything 0 then do something)
-           -something appears in parts of 2 --> model as graph
-           -assume a greedy then try to show why it works
-           -find way to simplify into one variable if multiple exist
-           -treat it like fmc (note any passing thoughts/algo that could be used so you can revisit them)
-           -find lower and upper bounds on answer
-           -figure out what ur trying to find and isolate it
-           -see what observations you have and come up with more continuations
-           -work backwards (in constructive, go from the goal to the start)
-           -turn into prefix/suffix sum argument (often works if problem revolves around adjacent array elements)
-           -instead of solving for answer, try solving for complement (ex, find n-(min) instead of max)
-           -draw something
-           -simulate a process
-           -dont implement something unless if ur fairly confident its correct
-           -after 3 bad submissions move on to next problem if applicable
-           -do something instead of nothing and stay organized
-           -write stuff down
-        Random stuff to check when wa:
-           -if code is way too long/cancer then reassess
-           -switched N/M
-           -int overflow
-           -switched variables
-           -wrong MOD
-           -hardcoded edge case incorrectly
-        Random stuff to check when tle:
-           -continue instead of break
-           -condition in for/while loop bad
-        Random stuff to check when rte:
-           -switched N/M
-           -long to int/int overflow
-           -division by 0
-           -edge case for empty list/data structure/N=1
-     */
-
-/************************/
+string s, ab, ac, bc, ba, ca, cb;
+ll n;
 
 void solve() {
+  ll l, r; cin >> l >> r;
+  ll a, b, c, d, e, f;
+  a = b = c = d = e = f = 0;
+  rep(i, l-1, r) {
+    if(ab[i]!=s[i]) {
+      a++;
+    }
+    if(ac[i]!=s[i]) {
+      b++;
+    }
+    if(ba[i]!=s[i]) {
+      c++;
+    }
+    if(bc[i]!=s[i]) {
+      d++;
+    }
+    if(ca[i]!=s[i]) {
+      e++;
+    }
+    if(cb[i]!=s[i]) {
+      f++;
+    }
+  }
 
+  cout << min(a,min(b,min(c,min(d,min(e,f))))) << endl;
 }
 
 int main() {
@@ -150,17 +65,39 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
-  /*
-  // for multiple testcase problems
+  ll m; cin >> n >> m;
+  cin >> s;
 
-  ll t; cin >> t;
-  while(t--) {
-    solve();
+  rep(i, 0, n) {
+    if(i%3==0) {
+      ab.push_back('a');
+      ac.push_back('a');
+      bc.push_back('b');
+      ba.push_back('b');
+      ca.push_back('c');
+      cb.push_back('c');
+    }
+    if(i%3==1) {
+      ab.push_back('b');
+      ac.push_back('c');
+      bc.push_back('c');
+      ba.push_back('a');
+      ca.push_back('a');
+      cb.push_back('b');
+    }
+    if(i%3==2) {
+      ab.push_back('c');
+      ac.push_back('b');
+      bc.push_back('a');
+      ba.push_back('c');
+      ca.push_back('b');
+      cb.push_back('a');
+    }
   }
 
-  */
-
-  solve();
+  while(m--) {
+    solve();
+  }
 
   return 0;
 }
