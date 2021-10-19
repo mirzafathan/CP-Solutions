f1fc753ec671709b026527afe9221547de4103fa
Mirza Fathan <muhammad.arsyad@tiket.com>
2021-10-19 11:39:48 +0700

    hmm

--- LeetCode/10.cpp
+++ LeetCode/10.cpp
@@ -6,22 +6,50 @@ bool isMatch(string s, string p) {
   long long len_s = s.length();
   long long len_p = p.length();
 
-  bool dp[len_s][len_p];
+  bool dp[len_s+1][len_p+1];
 
-  if(s[0]!=p[0] && p[0]!='.') return false;
-  else dp[0][0] = true;
+  dp[0][0] = true;
+  dp[0][1] = false;
 
-  long long j = 1;
-  for(long long i=1; i<len_s; i++) {
-    if(p[j]=='*') {
-      if(j>1) {
-        
+  for(long long i = 1; i<len_p; i++) {
+    if(p[i]!='*') {
+      if(p[i-1]!='*') {
+        for(long long j=0; j<len_p; j++) {
+          dp[0][j+1] = false;
+        }
+        break;
+      }
+    } else {
+      dp[0][i+1] = true;
+    }
+  }
+
+  for(long long i = 0; i<len_s; i++) {
+    for(long long j = 0; j<len_p; j++) {
+      if(p[j]=='*') {
+        if(p[j-1]==s[i] || (p[j-1]==s[i] && s[i]==s[i-1])) {
+          dp[i+1][j+1] = dp[i][j+1];
+        } else if(j>1) {
+          dp[i+1][j+1] = dp[i+1][j-1];
+        }
+      } else if((p[j]=='.') || (p[j]==s[i])) {
+        dp[i+1][j+1] = dp[i][j];
       } else {
-
+        dp[i+1][j+1] = false;
       }
     }
   }
+
+  return dp[len_s][len_p];
 }
+
+/* debug
+  for(long long i=0; i<=len_s; i++) {
+    for(long long j=0; j<=len_p; j++) {
+      cout << dp[i][j] << " ";
+    } cout << endl;
+  }
+*/
 
 int main() {
   ios_base::sync_with_stdio(0);
