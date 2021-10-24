1c3a414bc9590a1bcb3e0d416a48bdddb324170f
Mirza Fathan <muhammad.arsyad@tiket.com>
2021-10-24 22:25:51 +0700

    sudah ac

--- LeetCode/51.cpp
+++ LeetCode/51.cpp
@@ -7,14 +7,51 @@ class Solution {
   vector< vector<string> > solveNQueens(int n) {
 
     vector< vector<string> > solution;
-/*
-    vector< vector<string> > solution;
-    for(int i=0; i<queens.size(); i++) {
-      for(int j=0; j<queens[i].size(); j++) {
-        cout << queens[i].first << " " << queens[i].second << endl;
+    vector<string> queens;
+    for(int i=0; i<n; i++) {
+      string temp;
+      for(int j=0; j<n; j++) {
+        temp.push_back('.');
       }
-    }*/
+      queens.push_back(temp);
+    }
+    solveNQueens(solution, queens, 0, n);
+
     return solution;
+  }
+  private:
+  void solveNQueens(vector< vector<string> > &solution, vector<string> &queens, int row, int n) {
+    if(row==n) {
+      solution.push_back(queens);
+      return;
+    }
+
+    for(int i=0; i<n; i++) {
+      if(isValid(queens, row, i)) {
+        queens[row][i] = 'Q';
+        solveNQueens(solution, queens, row+1, n);
+        queens[row][i] = '.';
+      }
+    }
+
+  }
+  bool isValid(vector<string> queens, int row, int col) {
+    int sum = row+col;
+    int dif = row-col;
+
+    for(int i=0; i<row; i++) {
+      if(queens[i][col]=='Q') return false;
+      
+      if((sum-i)>=0) {
+        if(queens[i][sum-i] == 'Q') return false;
+      }
+      
+      if((i-dif)>=0) {
+        if(queens[i][i-dif] == 'Q') return false;
+      }
+    }
+
+    return true;
   }
 };
 
@@ -22,6 +59,17 @@ int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
+
+  int n; cin >> n;
+  Solution solve;
+  vector< vector<string> > solutions = solve.solveNQueens(n);
+  
+  for(int i=0; i<solutions.size(); i++) {
+    for(int j=0; j<solutions[i].size(); j++) {
+      cout << solutions[i][j] << " ";
+    }
+    cout << endl;
+  }
 
   return 0;
 }
