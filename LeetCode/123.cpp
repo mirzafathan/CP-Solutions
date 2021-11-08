b18ee51ce960847a69e05a2bd1073f24e4c0c333
Mirza Fathan <muhammad.arsyad@tiket.com>
2021-11-09 05:47:04 +0700

    123 init

---
+++ LeetCode/123.cpp
@@ -0,0 +1,14 @@
+// Muhammad Mirza Fathan 2021
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+};
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  return 0;
+}
