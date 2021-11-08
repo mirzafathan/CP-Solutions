13b4828555e11dbcabf6faa6b4c9e466f75e7c15
Mirza Fathan <muhammad.arsyad@tiket.com>
2021-11-09 05:57:25 +0700

    undone

--- LeetCode/123.cpp
+++ LeetCode/123.cpp
@@ -3,12 +3,35 @@
 using namespace std;
 
 class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+      int n = prices.size();
+      int dp[3][n];
+      for(int i=0; i<n; i++) dp[0][i] = 0;
+      for(int k=1; k<=2; k++) {
+        for(int i=0; i<n; i++) {
+          
+        }
+      }
+    }
 };
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
+
+  vector<int> v;
+
+  int in;
+  while(true) {
+    cin >> in;
+    if(in<0) break;
+    v.push_back(in);
+  }
+
+  Solution solve;
+  cout << solve.maxProfit(v) << endl;
 
   return 0;
 }
