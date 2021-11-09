f7b2bcb81aaa92c7431f7460d19a11e5ff9dafaa
Mirza Fathan <muhammad.arsyad@tiket.com>
2021-11-09 23:30:12 +0700

    164

---
+++ LeetCode/164.cpp
@@ -0,0 +1,19 @@
+// Muhammad Mirza Fathan 2021
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+      int len = nums.size();
+      
+    }
+};
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  return 0;
+}
