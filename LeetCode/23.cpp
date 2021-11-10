8efecfdf5f337804dc12bdbb4133eb68be475cb9
Mirza Fathan <muhammad.arsyad@tiket.com>
2021-11-10 14:07:45 +0700

    23

---
+++ LeetCode/23.cpp
@@ -0,0 +1,28 @@
+// Muhammad Mirza Fathan 2021
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+     int val;
+     ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+    }
+};
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+
+
+  return 0;
+}
