fe2323cb443020969554da5865e29c779585b3ab
mirzaalarsyad <13518111@std.stei.itb.ac.id>
2024-10-22 21:49:57 +0700

    dice combinations

--- CSES.FI/DP/dice.cpp
+++ CSES.FI/DP/dice.cpp
@@ -3,18 +3,27 @@
 using namespace std;
 typedef long long ll;
 # define INF8 (ll)(1e17+5)
-const int mod = 1e9+7
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-
+  ll mod = ((ll) 1e9+7);
   ll n; cin >> n;
-  vector<ll> combinations(n+1,0);
-  combinations[0] = 1;
+  ll dp[1000000+7];
+  dp[0] = 1;
+  dp[1] = dp[0];
+  dp[2] = dp[1] + dp[0];
+  dp[3] = dp[2] + dp[1] + dp[0];
+  dp[4] = dp[3] + dp[2] + dp[1] + dp[0];
+  dp[5] = dp[4] + dp[3] + dp[2] + dp[1] + dp[0];
+  dp[6] = dp[5] + dp[4] + dp[3] + dp[2] + dp[1] + dp[0];
 
-  for(int i=1; i<=n; i++) 
+  for(ll i=7; i<=n; i++) {
+    dp[i] = (dp[i-1] + dp[i-2] + dp[i-3] + dp[i-4] + dp[i-5] + dp[i-6]) % mod;
+  }
+
+  cout << dp[n] << endl;
 
   return 0;
 }
