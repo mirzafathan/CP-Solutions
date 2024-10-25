0537cbc6eccdb3deedbaa6ce879291889dbff88f
mirzaalarsyad <13518111@std.stei.itb.ac.id>
2024-10-26 02:25:10 +0700

    1636: Coin Combinations II

---
+++ CSES.FI/DP/1636.cpp
@@ -0,0 +1,35 @@
+// Muhammad Mirza Fathan | 2021
+// Coin Combinations II
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+# define INF8 (ll)(1e17+5)
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int n, x;
+  cin >> n >> x;
+
+  int c[105];
+  for(int i = 0; i < n; i++) {
+    cin >> c[i];
+  }
+
+  ll dp[1000000 + 5] = {};
+  dp[0] = 1;
+
+  for(int i = 0; i < n; i++) {
+    for(int j = c[i]; j <= x; j++) {
+      dp[j] = dp[j] + dp[j - c[i]];
+      dp[j] = dp[j] % ((ll) 1e9 + 7);
+    }
+  }
+
+  cout << dp[x] << endl;
+
+  return 0;
+}
